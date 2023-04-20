385503ed5ecb20faf33b748f4a7d6db13d75d785
DuyVo96 <vominhduy.cokhi@gmail.com>
2023-04-20 17:59:18 +0700

    change metadata

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xc5DDAdBEE65558dD174DBCd43362944EEe5746aC";
 
-const METADATA_URL = "https://nft-celestia.vercel.app/api/";
+const METADATA_URL = "https://celestia-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
