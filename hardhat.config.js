6045489be9ad3ae306e23aad15f0cbe5f898e4ec
DuyVo96 <vominhduy.cokhi@gmail.com>
2023-04-26 11:25:15 +0700

    change something

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const HTTP_URL = process.env.HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     ethermint: {
-      url: QUICKNODE_HTTP_URL,
+      url: HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
   },
